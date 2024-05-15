4ccc5c763e650ebf30181847dcf8e61a8a411532
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-05-15 20:11:30 +0300

    ISAPP-67: pipeline fix

--- src/main/java/vsu/cs/is/infsysserver/employee/EmployeeService.java
+++ src/main/java/vsu/cs/is/infsysserver/employee/EmployeeService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import vsu.cs.is.infsysserver.employee.adapter.EmployeeMapper;
 import vsu.cs.is.infsysserver.employee.adapter.jpa.EmployeeRepository;
 import vsu.cs.is.infsysserver.employee.adapter.jpa.entity.Employee;
-import vsu.cs.is.infsysserver.employee.adapter.rest.dto.request.CreateEmptyLessonsRequest;
 import vsu.cs.is.infsysserver.employee.adapter.rest.dto.request.EmployeeCreateRequest;
 import vsu.cs.is.infsysserver.employee.adapter.rest.dto.request.EmployeeUpdateRequest;
 import vsu.cs.is.infsysserver.employee.adapter.rest.dto.response.EmployeeAdminResponse;
