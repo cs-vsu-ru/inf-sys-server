e2db3dbda5335e12cc14a75f04916ac98facfaa6
r.pavel <tokichiihere@gmail.com>
2024-05-16 12:02:47 +0300

    ISAPP-88: поменял default val у static pages entity

--- src/main/resources/db/changelog/changeset/202405091612_static_pages.sql
+++ src/main/resources/db/changelog/changeset/202405091612_static_pages.sql
@@ -7,7 +7,7 @@ create sequence static_pages_sequence;
 create table static_pages
 (
     id                   bigint not null
-        primary key,
+        primary key default nextval('static_pages_sequence'),
     content_about        text,
     content_confidential text,
     content_contacts     text,
