fa61d213c8ce5f8000b2178a2705c75b36237bf2
r.pavel <tokichiihere@gmail.com>
2024-05-23 01:59:10 +0300

    ISAPP-95: поменял путь у переменных

--- src/main/java/vsu/cs/is/infsysserver/configuration/properties/ApplicationProperties.java
+++ src/main/java/vsu/cs/is/infsysserver/configuration/properties/ApplicationProperties.java
@@ -2,7 +2,7 @@ package vsu.cs.is.infsysserver.configuration.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "application")
+@ConfigurationProperties(prefix = "application.upload")
 public record ApplicationProperties(
         String serverUrl,
         String baseFilesFolder
