386f508fa4f94d59bc9f8a7a3846851df6b53d4b
r.pavel <tokichiihere@gmail.com>
2024-05-25 00:58:58 +0300

    ISAPP-98: добавил CORS config

--- src/main/java/vsu/cs/is/infsysserver/user/adapter/rest/UserController.java
+++ src/main/java/vsu/cs/is/infsysserver/user/adapter/rest/UserController.java
@@ -3,7 +3,6 @@ package vsu.cs.is.infsysserver.user.adapter.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import static org.springframework.http.ResponseEntity.ok;
 @RequestMapping("/api/account")
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin
 public class UserController implements UserApi {
 
     private final EmployeeService employeeService;
