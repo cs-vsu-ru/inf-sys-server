f005ba55ec130f6040077d79f1b51c3fd2e0c744
Анна Гурьева <anna.gureva@travelata.ru>
2026-04-16 14:21:02 +0300

    add slider description

--- src/main/java/vsu/cs/is/infsysserver/slider/adapter/rest/dto/response/SliderElementResponse.java
+++ src/main/java/vsu/cs/is/infsysserver/slider/adapter/rest/dto/response/SliderElementResponse.java
@@ -8,6 +8,8 @@ public record SliderElementResponse(
         @Schema(description = "Заголовок элемента слайдера", example = "Лаборатория электротехники, "
                 + "электроники и схемотехники")
         String title,
+        @Schema(description = "Описание элемента слайдера", example = "Описание")
+        String description,
         @Schema(description = "Ссылка на изображение элемента слайдера", example = "https://cs.vsu.ru/image/123")
         String imageURL,
         @Schema(description = "Ссылка на страницу")
